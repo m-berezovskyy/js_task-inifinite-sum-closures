7bed5061df6c7caa1164c4c5594aedf3e614dc1a
Marian <mberez999@gmail.com>
2026-06-03 05:00:08 +0300

    add solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,23 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  function adder(num) {
+    if (num !== undefined) {
+      sum += num;
+
+      return adder;
+    } else {
+      const finalSum = sum;
+
+      sum = 0;
+
+      return finalSum;
+    }
+  }
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
